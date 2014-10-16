9b7a7c387dd838870eaf3d1e421aa841915848d5
Mathijs Kadijk <mkadijk@gmail.com>
2014-10-16 13:28:28 +0200

    Test NSPersistentStoreCoordinator

---
+++ CoreDataKitTests/NSPersistentStoreCoordinatorTests.swift
@@ -0,0 +1,31 @@
+//
+//  NSPersistentStoreCoordinatorTests.swift
+//  CoreDataKit
+//
+//  Created by Mathijs Kadijk on 16-10-14.
+//  Copyright (c) 2014 Mathijs Kadijk. All rights reserved.
+//
+
+import XCTest
+import CoreData
+import CoreDataKit
+
+class NSPersistentStoreCoordinatorTests: TestCase {
+    func testInitializationSQLiteStore() {
+        let optionalCoordinator = NSPersistentStoreCoordinator(automigrating: true)
+        XCTAssertNotNil(optionalCoordinator, "Missing coordinator")
+
+        if let coordinator = optionalCoordinator {
+            XCTAssertTrue(coordinator.persistentStores.count > 0, "Missing persistent store")
+        }
+    }
+
+    func testInitializationMemoryStore() {
+        let optionalCoordinator = NSPersistentStoreCoordinator(automigrating: true)
+        XCTAssertNotNil(optionalCoordinator, "Missing coordinator")
+
+        if let coordinator = optionalCoordinator {
+            XCTAssertTrue(coordinator.persistentStores.count > 0, "Missing persistent store")
+        }
+    }
+}
