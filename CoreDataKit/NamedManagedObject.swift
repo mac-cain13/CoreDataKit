f3ecedb370c9a7f2986f2700ab8b92771b411eb3
Mathijs Kadijk <mkadijk@gmail.com>
2014-10-17 15:43:38 +0200

    Document NamedManagedObject protocol

--- CoreDataKit/NamedManagedObject.swift
+++ CoreDataKit/NamedManagedObject.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Mathijs Kadijk. All rights reserved.
 //
 
+/// Protocol that enables CoreDataKit to handle entities based on a
 public protocol NamedManagedObject {
+    /// The name of the entity as it is known in the managed object model
     class var entityName: String { get }
 }
